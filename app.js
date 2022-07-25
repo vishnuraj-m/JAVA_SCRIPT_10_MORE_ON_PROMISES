75b656eb7f9426ba889bf2f53e5d3d5be2d76e08
vishnuacappella <acappella.studiov@gmail.com>
2022-07-25 18:53:58 +0530

    files created

---
+++ app.js
@@ -0,0 +1,3 @@
+import getData from "./index.js"
+const response = getData();
+console.log(response);
---
+++ index.js
@@ -0,0 +1,11 @@
+const makeMeAPromise = () => new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve({
+            car: 'BMW',
+            color: 'red',
+            year: '2018'
+        });
+    }, 700)
+})
+
+export default makeMeAPromise;
